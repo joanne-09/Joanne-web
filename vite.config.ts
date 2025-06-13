fd722dcc239c4e0a519403eb6a9b34352ef860ec
Joanne <joanne.zh2015@gmail.com>
2025-06-13 22:05:58 +0800

    change repo name in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/joanne-09.github.io/',
+  base: '/Joanne-web/',
   plugins: [react()],
 })
